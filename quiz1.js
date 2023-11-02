371e3660c1999dca85b1fae3e9e698c0fcc86d17
DODI ZEPANYA MANURUNG <149552320+DODIZEPANYA@users.noreply.github.com>
2023-11-02 14:21:13 +0700

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,13 @@
+const restaurant = {
+  name: "DODI ZEPANYA RESTO & CAFE",
+  city: "SIDAMANIK",
+  favoriteDrink: "MINUMAN RASA TARO",
+  favoriteFood: "NASI GORENG SEAFOOD",
+  isVegan: true,
+};
+
+const name = restaurant.name;
+const favoriteDrink = restaurant.favoriteDrink;
+
+console.log("Nama Restoran:", restaurant.name);
+console.log("Minuman Favorit:", restaurant.favoriteDrink);
